5fd73253e9c353917162741c893b51c192d50c6f
Mike Chernev <mike@mikechernev.com>
2015-05-10 18:35:05 +0200

    [FORMAT] Code beautifying

--- ajax/get_dbs.php
+++ ajax/get_dbs.php
@@ -1,15 +1,15 @@
 <?php
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])
-    && ! empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+    && !empty($_SERVER['HTTP_X_REQUESTED_WITH'])
     && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
 ) {
     header('Content-Type: application/json');
 
-    $response = array(
-    'success' => false,
-    );
+    $response = [
+        'success' => false,
+    ];
 
-    if (! class_exists('\Redis')) {
+    if (!class_exists('\Redis')) {
         $response['error'] = 'The Redis PHP extention is required!';
         echo json_encode($response);
         exit;
@@ -20,7 +20,7 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])
 
     $dbs = array_flip(preg_grep("/db\d+/", array_keys($redis->info())));
 
-    $selected_db = (isset($_COOKIE['selected_db']) && ! empty($_COOKIE['selected_db'])) ? $_COOKIE['selected_db'] : 'db0';
+    $selected_db = (isset($_COOKIE['selected_db']) && !empty($_COOKIE['selected_db'])) ? $_COOKIE['selected_db'] : 'db0';
     $dbs[$selected_db] = 'selected';
     $response['success'] = true;
     $response['result'] = $dbs;
--- ajax/get_key_value.php
+++ ajax/get_key_value.php
@@ -1,15 +1,15 @@
 <?php
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])
-    && ! empty($_SERVER['HTTP_X_REQUESTED_WITH'])
+    && !empty($_SERVER['HTTP_X_REQUESTED_WITH'])
     && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
 ) {
     header('Content-Type: application/json');
 
-    $response = array(
-    'success' => false,
-    );
+    $response = [
+        'success' => false,
+    ];
 
-    if (! class_exists('\Redis')) {
+    if (!class_exists('\Redis')) {
         $response['error'] = 'The Redis PHP extention is required!';
         echo json_encode($response);
         exit;
@@ -24,11 +24,10 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])
 
     $redis = new \Redis();
     $redis->pconnect('localhost');
-    $selected_db = (isset($_COOKIE['selected_db']) && ! empty($_COOKIE['selected_db'])) ? $_COOKIE['selected_db'] : 'db0';
+    $selected_db = (isset($_COOKIE['selected_db']) && !empty($_COOKIE['selected_db'])) ? $_COOKIE['selected_db'] : 'db0';
     $redis->select(trim($selected_db, 'db'));
 
-    switch ($redis->type($key))
-    {
+    switch ($redis->type($key)) {
     case Redis::REDIS_STRING:
         $response['result'] = $redis->get($key);
         break;
